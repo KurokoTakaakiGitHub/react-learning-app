3b9bacba2bd5d997e7ad8369a3ba19d4cf65d925
KurokoTakaakiGitHub <66106869+KurokoTakaakiGitHub@users.noreply.github.com>
2022-03-02 06:11:20 +0900

    face show changed on/off button

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,33 @@ import React, { useState } from "react";
 import ColorfulMessage from "./Components/ColorfulMessage";
 
 const App = () => {
-  // ボタンクリックイベント
-  const onClickCountUpCountUp = () => {
-    setNum(num + 1);
-  };
   // state 変数, 関数 (初期値)
   const [num, setNum] = useState(0);
+
+  // faceShow
+  const [faceShowFlag, changeFaceShowFlg] = useState(false);
+
+  // カウントアップボタン クリックイベント
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+
+  // 顔文字on/offボタン クリックイベント
+  const onClickFaceOnOff = () => {
+    changeFaceShowFlg(!faceShowFlag);
+  };
 
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは!</h1>
       <ColorfulMessage color="blue" message="お元気ですか" />
       <ColorfulMessage color="pink" message="元気です" />
-      <button onClick={onClickCountUpCountUp}>カウントアップ！</button>
+      <button onClick={onClickCountUp}>カウントアップ！</button>
+      <br />
+      <button onClick={onClickFaceOnOff}>顔文字 on/off</button>
+      <br />
       <p>{num}</p>
+      <p>{faceShowFlag && "(^ ^)"}</p>
     </>
   );
 };
